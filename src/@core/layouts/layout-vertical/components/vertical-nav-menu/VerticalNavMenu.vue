2a29f88bd52f1f1d6e2d824244344a5dc4a4a8ab
Ivan <103099590+ivangrapher@users.noreply.github.com>
2022-10-09 15:56:37 +0900

    Update VerticalNavMenu.vue

--- src/@core/layouts/layout-vertical/components/vertical-nav-menu/VerticalNavMenu.vue
+++ src/@core/layouts/layout-vertical/components/vertical-nav-menu/VerticalNavMenu.vue
@@ -35,24 +35,6 @@
               </h2>
             </b-link>
           </li>
-
-          <!-- Toggler Button -->
-          <li class="nav-item nav-toggle">
-            <b-link class="nav-link modern-nav-toggle">
-              <feather-icon
-                icon="XIcon"
-                size="20"
-                class="d-block d-xl-none"
-                @click="toggleVerticalMenuActive"
-              />
-              <feather-icon
-                :icon="collapseTogglerIconFeather"
-                size="20"
-                class="d-none d-xl-block collapse-toggle-icon"
-                @click="toggleCollapsed"
-              />
-            </b-link>
-          </li>
         </ul>
       </slot>
     </div>
